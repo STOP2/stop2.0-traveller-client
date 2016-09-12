706c97a01b2a6c5421ec3c924d5b23691765b6e4
Milla Peltoniemi <tunturikissa@gmail.com>
2016-09-12 15:08:18 +0300

    index.android.js modified

--- index.android.js
+++ index.android.js
@@ -1,38 +1,6 @@
-/* @flow */
+'use strict';
 
-import React, { Component } from 'react';
+import React { AppRegistry } from 'react-native';
+import App from '.app/containers/app';
 
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-
-class Stop2TravellerClient extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.title}>
-          Stop 2.0 Traveller Client
-        </Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  title: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  }
-});
-
-AppRegistry.registerComponent('Stop2TravellerClient', () => Stop2TravellerClient);
+AppRegistry.registerComponent('Stop2TravellerClient', () =>App);
