8c5fbf41a1f473d9b0c21f662d2f3f1fad89dfac
avrj <anraiha@cs.helsinki.fi>
2016-09-12 19:47:37 +0300

    iOS version shares now the same root component as android version

--- index.ios.js
+++ index.ios.js
@@ -1,38 +1,4 @@
-/* @flow */
+import React, { AppRegistry } from 'react-native';
+import App from './app/containers/app.js';
 
-import React, { Component } from 'react';
-
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-
-class Stop2TravellerClient extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.title}>
-          Stop 2.0 Traveller Client
-        </Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  title: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  }
-});
-
-AppRegistry.registerComponent('Stop2TravellerClient', () => Stop2TravellerClient);
+AppRegistry.registerComponent('Stop2TravellerClient', () => App);
