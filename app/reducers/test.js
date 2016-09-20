ecc4c6088bd4f89a7770bbc963e0d48c78902e93
avrj <anraiha@cs.helsinki.fi>
2016-09-20 13:05:00 +0300

    reducer for fetching departures

--- app/reducers/test.js
+++ app/reducers/test.js
@@ -1,5 +1,23 @@
-const test = (state = [], action) => {
-  return state;
+import { REQUEST_DEPARTURES, RECEIVE_DEPARTURES } from '../actions/fetchDeparturesActions'
+
+let initialState = {
+  stop: [],
+  isFetching: false
+}
+
+const test = (state = initialState, action) => {
+  switch (action.type) {
+    case REQUEST_DEPARTURES:
+      return Object.assign({}, state, {isFetching: true});
+    break
+
+    case RECEIVE_DEPARTURES:
+      return Object.assign({}, state, {isFetching: false, stop: action.departures[0].stop});
+    break
+
+    default:
+      return state
+  }
 }
 
 export default test
