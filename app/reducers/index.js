0dd66a2dc378e283481247f0ccc4c2f680495278
avrj <anraiha@cs.helsinki.fi>
2016-09-20 14:14:47 +0300

    rename test -> fetch

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,10 +1,10 @@
 import { combineReducers } from 'redux'
 
-import test from './test'
+import fetch from './fetch'
 import routes from './routes'
 
 const reducers = combineReducers({
-  test,
+  fetch,
   routes
 })
 
