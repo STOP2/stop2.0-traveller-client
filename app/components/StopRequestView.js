2ecd163f1e4c1cd0b1b14176e8093fd6c0ca9349
lapula <pulliainen.laur@gmail.com>
2016-09-27 13:58:34 +0300

    POST toimii

--- app/components/StopRequestView.js
+++ app/components/StopRequestView.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Text, ListView, View, TouchableOpacity } from 'react-native'
 import { Actions } from 'react-native-router-flux'
+import { sendStoprequest } from '../actions/sendStoprequest'
 
 import styles from '../styles/stylesheet'
 import strings from '../resources/translations'
@@ -15,8 +16,13 @@ class StopRequestView extends Component{
     }
   }
 
+  componentWillReceiveProps = (nextProps) => {
+    console.log(nextProps.sent)
+  }
+
   render() {
     const goBack = () => Actions.departures()
+    const sendStoprequest = () => this.props.sendStoprequest("1234", this.props.stop.stopId, "stop")
 
     const vehicleTypes = 'tram metro train bus ferry'.split(' ')
 
@@ -24,6 +30,9 @@ class StopRequestView extends Component{
       <View style={styles.stopRequestStyle}>
       <View>
         <Text style={{fontSize: 20, textAlign: 'center'}}>{strings[vehicleTypes[this.props.vehicle.vehicle_type]]} {this.props.vehicle.line} to {this.props.vehicle.destination} {strings.stopsAt} {this.props.stop.stopName} ({this.props.stop.stopId})</Text>
+        <TouchableOpacity onPress={sendStoprequest}>
+          <Text style={{color: '#ff0000', textAlign: 'center'}}>{strings.back}</Text>
+        </TouchableOpacity>
         <TouchableOpacity onPress={goBack}>
           <Text style={{color: '#ff0000', textAlign: 'center'}}>{strings.back}</Text>
         </TouchableOpacity>
@@ -33,4 +42,18 @@ class StopRequestView extends Component{
   }
 }
 
-export default StopRequestView
+const mapStateToProps = (state) => {
+  return {
+    sent: state.fetchReducer.isSending
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+      sendStoprequest: (bus_id, stop_id, request_type) => {
+        dispatch(sendStoprequest(bus_id, stop_id, request_type))
+      }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(StopRequestView);
