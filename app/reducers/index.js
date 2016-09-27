e78d6e9b478d7a59f499f35cf4043dc8eb6a5760
avrj <anraiha@cs.helsinki.fi>
2016-09-27 14:58:09 +0300

    added locationReducer

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux'
 
 import fetchReducer from './fetchReducer'
+import locationReducer from './locationReducer'
 import routes from './routes'
 
 const reducers = combineReducers({
   fetchReducer,
+  locationReducer,
   routes
 })
 
