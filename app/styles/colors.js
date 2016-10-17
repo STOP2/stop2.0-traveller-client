9942d9217cdca3b19eb88e0dd6ba26dbcc83b463
Henna Ruohonen <henna.ruohonen@helsinki.fi>
2016-10-17 23:01:59 +0300

    color file

---
+++ app/styles/colors.js
@@ -0,0 +1,13 @@
+const colors {
+  busBlue: '#007AC9'
+  tramGreen: '#00985F'
+  trainViolet: '#8C4799'
+  metroOrange: '#FF6319'
+  ferryBlue: '#00B9E4'
+  lightFerryBlue: '#BEE4F8'
+  HSLpink: '#F092CD'
+  HSLlightPink: '#F4DEEA'
+  HSLgreen: '#64BE14'
+  
+}
+export default colors
