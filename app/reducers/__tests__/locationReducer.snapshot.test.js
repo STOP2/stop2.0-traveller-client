c6f372d78ea82c8bc911f974a56eb4fee48bde7d
avrj <anraiha@cs.helsinki.fi>
2016-10-22 16:32:00 +0300

    fixed eslint warnings

--- app/reducers/__tests__/locationReducer.snapshot.test.js
+++ app/reducers/__tests__/locationReducer.snapshot.test.js
@@ -1,12 +1,15 @@
 import locationReducer, { initialState } from '../../reducers/locationReducer'
 import { setLocation } from '../../actions/locationActions'
 
-describe('location reducer', () => {
-    it('returns the same state on an unhandled action', () => {
+describe('location reducer', () =>
+{
+    it('returns the same state on an unhandled action', () =>
+    {
         expect(locationReducer(initialState, {type: '_NULL'})).toMatchSnapshot()
     })
 
-    it('handles SET_LOCATION action', () => {
+    it('handles SET_LOCATION action', () =>
+    {
         let locationData = {coords: []}
         expect(locationReducer(initialState, setLocation(locationData))).toMatchSnapshot()
     })
