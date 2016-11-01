9ae5d2ea99a2c6c65f3e3ef46c0c567ab09bd95d
avrj <anraiha@cs.helsinki.fi>
2016-11-01 14:58:39 +0200

    added fcmReducer

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux'
 
 import fetchReducer from './fetchReducer'
 import locationReducer from './locationReducer'
+import fcmReducer from './fcmReducer'
 import routes from './routes'
 
 const reducers = combineReducers({
     fetchReducer,
     locationReducer,
+    fcmReducer,
     routes
 })
 
