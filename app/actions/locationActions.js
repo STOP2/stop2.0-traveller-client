002d96405c0d4cb34f6bf695c7a23e40e472e2fd
avrj <anraiha@cs.helsinki.fi>
2016-11-08 11:37:25 +0200

    moved gps permission checks to locationActions

--- app/actions/locationActions.js
+++ app/actions/locationActions.js
@@ -1,6 +1,39 @@
+import { checkPermission, requestPermission } from 'react-native-android-permissions'
+
 export const SET_LOCATION = 'SET_LOCATION'
 export const LOCATION_ERROR = 'LOCATION_ERROR'
 export const REQUEST_LOCATION = 'REQUEST_LOCATION'
+
+export const LOCATION_PERMISSION_ERROR = 'LOCATION_PERMISSION_ERROR'
+
+export let checkLocationPermissionAndGetLocation = function()
+{
+    return dispatch => {
+        checkPermission('android.permission.ACCESS_FINE_LOCATION').then(() => {
+            dispatch(getLocation())
+        }, () => {
+            setTimeout(() => {
+                requestPermission('android.permission.ACCESS_FINE_LOCATION').then(() => {
+                    dispatch(getLocation())
+                }, (error) => {
+                    dispatch(locationPermissionError(error))
+                })
+                // for the correct StatusBar behaviour with translucent={true} we need to wait a bit and ask for permission after the first render cycle
+                // (check https://github.com/facebook/react-native/issues/9413 for more info)
+            }, 0)
+        })
+    }
+}
+
+export let locationPermissionError = function(error)
+{
+    return {
+        type: LOCATION_PERMISSION_ERROR,
+        error: error
+    }
+}
+
+
 
 export let setLocation = function(locationData)
 {
