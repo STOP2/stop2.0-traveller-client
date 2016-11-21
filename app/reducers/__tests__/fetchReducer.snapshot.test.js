ce20c3792332a94a1ac05cbb1e307e27e7c593c8
avrj <anraiha@cs.helsinki.fi>
2016-11-21 14:58:06 +0200

    fixed tests

--- app/reducers/__tests__/fetchReducer.snapshot.test.js
+++ app/reducers/__tests__/fetchReducer.snapshot.test.js
@@ -26,13 +26,6 @@ describe('fetch reducer', () =>
         expect(fetchReducer(initialState, requestError('error'))).toMatchSnapshot()
     })
 
-    it('handles RECEIVE_DEPARTURES action', () =>
-    {
-        let departuresData = {"stops": [{"stop": {"schedule": [{"arrival": 7, "destination": "Rautatientori", "line": "55", "route_id": "HSL:1055", "trip_id": "HSL:1055_20161003_La_2_1412", "vehicle_type": 3}, {"arrival": 21, "destination": "Rautatientori", "line": "55", "route_id": "HSL:1055", "trip_id": "HSL:1055_20161003_La_2_1427", "vehicle_type": 3}], "stop_code": "3597", "stop_name": "A.I. Virtasen aukio"}}]}
-
-        expect(fetchReducer(initialState, receiveDepartures(departuresData))).toMatchSnapshot()
-    })
-
     it('handles RECEIVE_CONFIRM action', () =>
     {
         expect(fetchReducer(initialState, receiveConfirm())).toMatchSnapshot()
