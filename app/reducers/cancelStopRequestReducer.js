7da371854d27cea05e62cad5818153af5eaa592a
avrj <anraiha@cs.helsinki.fi>
2016-11-26 17:35:08 +0200

    separate cases for from and destination stops

--- app/reducers/cancelStopRequestReducer.js
+++ app/reducers/cancelStopRequestReducer.js
@@ -1,27 +1,67 @@
-import { STOPREQUEST_CANCELLED, SEND_STOPREQUEST_CANCELLATION, STOPREQUEST_CANCELLATION_ERROR } from '../actions/cancelStopRequest'
+import { STOPREQUEST_FROM_CANCELLED, STOPREQUEST_DESTINATION_CANCELLED, SEND_STOPREQUEST_FROM_CANCELLATION, SEND_STOPREQUEST_DESTINATION_CANCELLATION, STOPREQUEST_FROM_CANCELLATION_ERROR, STOPREQUEST_DESTINATION_CANCELLATION_ERROR } from '../actions/cancelStopRequest'
 
 export let initialState = {
-    stopRequestCancelled: false,
-    isCancellingStopRequest: false,
-    stopRequestCancellationError: null
+    stopRequestFromCancelled: false,
+    stopRequestDestinationCancelled: false,
+    isCancellingFromStopRequest: false,
+    isCancellingDestinationStopRequest: false,
+    stopRequestFromCancellationError: null,
+    stopRequestDestinationCancellationError: null
 }
 
 const fetchReducer = (state = initialState, action) =>
 {
     switch (action.type)
     {
-        case SEND_STOPREQUEST_CANCELLATION:
-            return Object.assign({}, state, {stopRequestCancelled: action.stopRequestCancelled,
-                isCancellingStopRequest: action.isCancellingStopRequest})
+        case SEND_STOPREQUEST_FROM_CANCELLATION:
+            return Object.assign({}, state,
+                {
+                    stopRequestFromCancelled: action.stopRequestCancelled,
+                    isCancellingFromStopRequest: action.isCancellingStopRequest
+                }
+            )
 
-        case STOPREQUEST_CANCELLED:
-            return Object.assign({}, state, {stopRequestCancelled: action.stopRequestCancelled,
-                isCancellingStopRequest: action.isCancellingStopRequest})
+        case STOPREQUEST_FROM_CANCELLED:
+            return Object.assign({}, state,
+                {
+                    stopRequestFromCancelled: action.stopRequestCancelled,
+                    isCancellingFromStopRequest: action.isCancellingStopRequest
+                }
+            )
 
-        case STOPREQUEST_CANCELLATION_ERROR:
-            return Object.assign({}, state, {stopRequestCancelled: action.stopRequestCancelled,
-                isCancellingStopRequest: action.isCancellingStopRequest,
-                stopRequestCancellationError: action.stopRequestCancellationError})
+        case STOPREQUEST_FROM_CANCELLATION_ERROR:
+            return Object.assign({}, state,
+                {
+                    stopRequestFromCancelled: action.stopRequestCancelled,
+                    isCancellingFromStopRequest: action.isCancellingStopRequest,
+                    stopRequestFromCancellationError: action.stopRequestCancellationError
+                }
+            )
+
+        case SEND_STOPREQUEST_DESTINATION_CANCELLATION:
+            return Object.assign({}, state,
+                {
+                    stopRequestDestinationCancelled: action.stopRequestCancelled,
+                    isCancellingDestinationStopRequest: action.isCancellingStopRequest
+                }
+            )
+
+        case STOPREQUEST_DESTINATION_CANCELLED:
+            return Object.assign({}, state,
+                {
+                    stopRequestDestinationCancelled: action.stopRequestCancelled,
+                    isCancellingDestinationStopRequest: action.isCancellingStopRequest
+                }
+            )
+
+        case STOPREQUEST_DESTINATION_CANCELLATION_ERROR:
+            return Object.assign({}, state,
+                {
+                    stopRequestDestinationCancelled: action.stopRequestCancelled,
+                    isCancellingDestinationStopRequest: action.isCancellingStopRequest,
+                    stopRequestDestinationCancellationError: action.stopRequestCancellationError
+                }
+            )
 
         default:
             return state
