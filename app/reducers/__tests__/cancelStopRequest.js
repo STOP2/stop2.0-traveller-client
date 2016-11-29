21a0e0b44ef7d3e20bb29227833372234e186558
avrj <anraiha@cs.helsinki.fi>
2016-11-29 14:18:22 +0200

    fixed typos

--- app/reducers/__tests__/cancelStopRequest.js
+++ app/reducers/__tests__/cancelStopRequest.js
@@ -1,23 +1,23 @@
-import cancelstopRequest, { initialState } from '../../reducers/cancelstopRequest'
+import cancelStopRequestReducer, { initialState } from '../../reducers/cancelStopRequest'
 import { cancelStopRequest, receiveStopRequestCancellationConfirmation, requestStopRequestCancellation, stopRequestCancellationError } from '../../actions/cancelStopRequest'
 describe('cancelStopRequest reducer', () =>
 {
     it('returns the same state on an unhandled action', () =>
     {
-        expect(cancelstopRequest(initialState, {type: '_NULL'})).toMatchSnapshot()
+        expect(cancelStopRequestReducer(initialState, {type: '_NULL'})).toMatchSnapshot()
     })
 
     it('handles SEND_STOPREQUEST_CANCELLATION action', () =>
     {
-        expect(cancelstopRequest(initialState, cancelStopRequest(123))).toMatchSnapshot()
+        expect(cancelStopRequestReducer(initialState, cancelStopRequest(123))).toMatchSnapshot()
     })
 
     it('handles STOPREQUEST_CANCELLED action', () =>
     {
-        expect(cancelstopRequest(initialState, receiveStopRequestCancellationConfirmation())).toMatchSnapshot()
+        expect(cancelStopRequestReducer(initialState, receiveStopRequestCancellationConfirmation())).toMatchSnapshot()
     })
 
     it('handles STOPREQUEST_CANCELLATION_ERROR action', () => {
-        expect(cancelstopRequest(initialState, stopRequestCancellationError('error'))).toMatchSnapshot()
+        expect(cancelStopRequestReducer(initialState, stopRequestCancellationError('error'))).toMatchSnapshot()
     })
 })
