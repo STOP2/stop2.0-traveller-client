8646e2639cbf57c108b13a7ddd0b9c9ff80c445a
Pekka Jylhä-Ollila <jype@cs.helsinki.fi>
2016-12-08 13:10:44 +0200

    Format app/components

--- app/components/BusListHeader.js
+++ app/components/BusListHeader.js
@@ -1,32 +1,32 @@
-import React, { Component } from 'react'
-import { View } from 'react-native'
+import React, { Component } from 'react';
+import { View } from 'react-native';
 
-import {DefaultText} from '../components/textComponents'
+import { DefaultText } from '../components/textComponents';
 
-import styles from '../styles/stylesheet'
-import strings from '../resources/translations'
+import styles from '../styles/stylesheet';
+import strings from '../resources/translations';
 
 export default class BusListHeader extends Component {
-    render()
-    {
-        return (
-          <View style={styles.busrowheader}>
-            <DefaultText style={styles.busrowText}>{strings.type}</DefaultText>
-            <DefaultText style={styles.busrowText}>{strings.line}</DefaultText>
-            <DefaultText style={styles.busrowText2}>{strings.dest}</DefaultText>
-            <DefaultText style={styles.busrowText}>{strings.leaves}</DefaultText>
-          </View>)
-    }
+  render() {
+    return (
+      <View style={styles.busrowheader}>
+        <DefaultText style={styles.busrowText}>{strings.type}</DefaultText>
+        <DefaultText style={styles.busrowText}>{strings.line}</DefaultText>
+        <DefaultText style={styles.busrowText2}>{strings.dest}</DefaultText>
+        <DefaultText style={styles.busrowText}>{strings.leaves}</DefaultText>
+      </View>
+    );
+  }
 }
 
 export class RouteListHeader extends Component {
-    render()
-    {
-        return (
-          <View style={styles.busrowheader}>
-            <DefaultText style={styles.busrowText}>{strings.stopCode}</DefaultText>
-            <DefaultText style={styles.busrowText2}>{strings.stopName}</DefaultText>
-            <DefaultText style={styles.busrowText}>{strings.arrivesIn}</DefaultText>
-          </View>)
-    }
+  render() {
+    return (
+      <View style={styles.busrowheader}>
+        <DefaultText style={styles.busrowText}>{strings.stopCode}</DefaultText>
+        <DefaultText style={styles.busrowText2}>{strings.stopName}</DefaultText>
+        <DefaultText style={styles.busrowText}>{strings.arrivesIn}</DefaultText>
+      </View>
+    );
+  }
 }
