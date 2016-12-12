fc39e878fdc288e4eb8a44e0168a405656c02bb8
aorin <aorin@users.noreply.github.com>
2016-12-12 13:51:54 +0200

    eslint fix

--- app/actions/fetchVehiclesActions.js
+++ app/actions/fetchVehiclesActions.js
@@ -1,69 +1,60 @@
-import config from '../config/config'
+import config from '../config/config';
 
-export const REQUEST_VEHICLES = 'REQUEST_VEHICLES'
-export const RECEIVE_VEHICLES = 'RECEIVE_VEHICLES'
-export const REQUEST_VEHICLES_ERROR = 'REQUEST_VEHICLES_ERROR'
+export const REQUEST_VEHICLES = 'REQUEST_VEHICLES';
+export const RECEIVE_VEHICLES = 'RECEIVE_VEHICLES';
+export const REQUEST_VEHICLES_ERROR = 'REQUEST_VEHICLES_ERROR';
 
-const API_ENDPOINT = '/vehicles/beacons'
+const API_ENDPOINT = '/vehicles/beacons';
 
-export let requestVehicles = function(beacons)
-{
-    return {
-        type: REQUEST_VEHICLES,
-        isFetching: true,
-        beacons: beacons
-    }
-}
+export const requestVehicles = function (beacons) {
+  return {
+    type: REQUEST_VEHICLES,
+    isFetching: true,
+    beacons,
+  };
+};
 
-export let receiveVehicles = function(json)
-{
-    console.log('CALLING RECEIVE VEHICLES')
-    console.log(json)
-    return {
-        type: RECEIVE_VEHICLES,
-        isFetching: false,
-        isReady: true,
-        error: false,
-        departures: json.stops
-    }
-}
+export const receiveVehicles = function (json) {
+  console.log('CALLING RECEIVE VEHICLES');
+  console.log(json);
+  return {
+    type: RECEIVE_VEHICLES,
+    isFetching: false,
+    isReady: true,
+    error: false,
+    departures: json.stops,
+  };
+};
 
-export let requestVehiclesError = function()
-{
-    console.log('FETCH ERRORED')
-    return {
-        type: REQUEST_VEHICLES_ERROR,
-        error: true
-    }
-}
+export const requestVehiclesError = function () {
+  console.log('FETCH ERRORED');
+  return {
+    type: REQUEST_VEHICLES_ERROR,
+    error: true,
+  };
+};
 
-export let fetchVehicles = function(beacons)
-{
-    return dispatch =>
-    {
-        console.log('DOING THE FETCH')
-        console.log(JSON.stringify({beacons}))
-        return fetch(config.API_URL + API_ENDPOINT,
-            {
-                method: 'POST',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({beacons})
-            })
-            .then(response =>
-              {
-                if (response.ok)
-                {
-                    return response.json()
-                }
-                else
-                {
-                    dispatch(requestVehiclesError())
-                }
+export const fetchVehicles = function (beacons) {
+  return (dispatch) => {
+    console.log('DOING THE FETCH');
+    console.log(JSON.stringify({ beacons }));
+    return fetch(config.API_URL + API_ENDPOINT,
+      {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ beacons }),
+      })
+            .then((response) => {
+              if (response.ok) {
+                return response.json();
+              } else {
+                dispatch(requestVehiclesError());
+              }
             })
             .then(json => dispatch(receiveVehicles(json)))
-            .catch(error => dispatch(requestVehiclesError(error)))
-    }
-}
+            .catch(error => dispatch(requestVehiclesError(error)));
+  };
+};
--- app/reducers/vehicles.js
+++ app/reducers/vehicles.js
@@ -1,40 +1,38 @@
-import { REQUEST_VEHICLES, RECEIVE_VEHICLES, REQUEST_VEHICLES_ERROR } from '../actions/fetchVehiclesActions'
-import {RESET_STATE} from '../actions/resetStateAction'
+import { REQUEST_VEHICLES, RECEIVE_VEHICLES, REQUEST_VEHICLES_ERROR } from '../actions/fetchVehiclesActions';
+import { RESET_STATE } from '../actions/resetStateAction';
 
-let initialState = {
-    vehicles: [],
-    isFetching: false,
-    isReady: false,
-    error: false
-}
+const initialState = {
+  vehicles: [],
+  isFetching: false,
+  isReady: false,
+  error: false,
+};
 
-const fetchDepartures = (state = initialState, action) =>
-{
-    switch (action.type)
-    {
+const fetchDepartures = (state = initialState, action) => {
+  switch (action.type) {
     case REQUEST_VEHICLES:
-        console.log('FETCHING VEHICLES')
-        return Object.assign({}, state, {isFetching: action.isFetching})
+      console.log('FETCHING VEHICLES');
+      return Object.assign({}, state, { isFetching: action.isFetching });
 
     case RECEIVE_VEHICLES:
-    console.log('RECEIVED VEHICLES')
-      console.log(actions.vehicles)
-        return Object.assign({}, state, {
-            isFetching: action.isFetching,
-            isReady: action.isReady,
-            error: action.error,
-            vehicles: action.vehicles
-        })
+      console.log('RECEIVED VEHICLES');
+      console.log(actions.vehicles);
+      return Object.assign({}, state, {
+        isFetching: action.isFetching,
+        isReady: action.isReady,
+        error: action.error,
+        vehicles: action.vehicles,
+      });
 
     case REQUEST_VEHICLES_ERROR:
-        return Object.assign({}, state, {error: action.error})
+      return Object.assign({}, state, { error: action.error });
 
     case RESET_STATE:
-        return Object.assign({}, state, initialState)
+      return Object.assign({}, state, initialState);
 
     default:
-        return state
-    }
-}
+      return state;
+  }
+};
 
-export default fetchDepartures
+export default fetchDepartures;
