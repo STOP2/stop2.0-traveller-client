8837d80ac8c628cd17999e9b07a0b8250a71b3d4
avrj <anraiha@cs.helsinki.fi>
2016-12-12 12:37:39 +0200

    test RECEIVE_DEPARTURES with beacons enabled

--- app/actions/__tests__/fetchDeparturesActions.snapshot.test.js
+++ app/actions/__tests__/fetchDeparturesActions.snapshot.test.js
@@ -26,4 +26,15 @@ describe('fetchDepartures actions', () =>
 
         expect(store.getActions()).toMatchSnapshot()
     })
+
+    it('should handle RECEIVE_DEPARTURES action with beacons enabled', async() =>
+    {
+        const response = '{"stops": [{"stop": {"schedule": [{"arrival": 7, "destination": "Rautatientori", "line": "55", "route_id": "HSL:1055", "trip_id": "HSL:1055_20161003_La_2_1412", "vehicle_type": 3}, {"arrival": 21, "destination": "Rautatientori", "line": "55", "route_id": "HSL:1055", "trip_id": "HSL:1055_20161003_La_2_1427", "vehicle_type": 3}], "stop_code": "3597", "stop_name": "A.I. Virtasen aukio"}}]}'
+
+        fetch.mockResponseSuccess(response)
+
+        await store.dispatch(fetchDepartures(60.203978, 24.9633573, true))
+
+        expect(store.getActions()).toMatchSnapshot()
+    })
 })
