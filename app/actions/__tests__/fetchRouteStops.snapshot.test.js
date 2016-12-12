056d786e695bbead4f056d1319d29672ab378d7c
avrj <anraiha@cs.helsinki.fi>
2016-12-12 12:38:37 +0200

    tests for REQUEST_ROUTE_STOPS_ERROR action

--- app/actions/__tests__/fetchRouteStops.snapshot.test.js
+++ app/actions/__tests__/fetchRouteStops.snapshot.test.js
@@ -11,6 +11,11 @@ beforeEach(() =>
 
 describe('fetchRouteStops actions', () =>
 {
+    it('should handle REQUEST_ROUTE_STOPS_ERROR action', () =>
+    {
+        expect(requestError(true)).toMatchSnapshot()
+    })
+
     it('should handle REQUEST_ROUTE_STOPS action', async() =>
     {
         const response = '{"stops": [{"stop": {"arrives_in": 4, "stop_code": 3028, "stop_id":"HSL:1240118", "stop_name":"Kumpulan kampus"}}]}'
