c02bb3cb264b42304e45782e3ca5d0b480d2b4bb
avrj <anraiha@cs.helsinki.fi>
2016-12-12 13:16:19 +0200

    added tests for REQUEST_BEACON_DATA and RESET_STATE actions

--- app/reducers/__tests__/beacons.snapshot.test.js
+++ app/reducers/__tests__/beacons.snapshot.test.js
@@ -1,5 +1,6 @@
 import beacons, { initialState } from '../../reducers/beacons'
-import { setBeaconData, beaconError } from '../../actions/beaconLocationActions'
+import { setBeaconData, beaconError, requestBeaconData } from '../../actions/beaconLocationActions'
+import { resetState } from '../../actions/resetStateAction'
 
 describe('beacon location reducer', () =>
 {
@@ -13,6 +14,17 @@ describe('beacon location reducer', () =>
         let beaconData = {beaconData: []}
         expect(beacons(initialState, setBeaconData(beaconData))).toMatchSnapshot()
     })
+
+    it('handles REQUEST_BEACON_DATA action', () =>
+    {
+        expect(beacons(initialState, requestBeaconData())).toMatchSnapshot()
+    })
+
+    it('handles RESET_STATE action', () =>
+    {
+        expect(beacons(initialState, resetState())).toMatchSnapshot()
+    })
+
 
     it('handles BEACON_ERROR action', () =>
     {
